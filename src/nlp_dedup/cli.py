285f91f86ab0137c8e3941f8a3eed492c67cb410
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-12-20 11:29:10 +0000

    feat: Add `cli` module

---
+++ src/nlp_dedup/cli.py
@@ -0,0 +1,13 @@
+"""Command line interface for deduplicating a text corpus."""
+
+import click
+
+
+@click.command()
+def main() -> None:
+    """Deduplicate a text corpus."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
