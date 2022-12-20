e9281b2af96bf7f0d775fad41576f6bf12932521
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-12-20 16:46:23 +0000

    chore: Import Deduper in __init__

--- src/nlp_dedup/__init__.py
+++ src/nlp_dedup/__init__.py
@@ -4,5 +4,7 @@
 
 import pkg_resources
 
+from .deduper import Deduper
+
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = pkg_resources.get_distribution("nlp_dedup").version
